ecd5e1f969891e91b0e7179b5fa947763fa663bf
brianmuhame <43782389+brianmuhame@users.noreply.github.com>
2018-11-07 16:15:08 +0300

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,29 @@
+def say_hello(): # function header in python
+    print("hello.")# function body
+    print("hi.")
+say_hello() # calling functions in python
+
+def why_goodbye():
+         print("I don't know why you say goodbye.")
+why_goodbye()
+
+name ='bryan'
+def greet_me():
+        print(f"Hello {name}") #f-strings
+greet_me()
+
+def add(a,b):
+        return a + b
+print(add(2,8))
+
+def minus(a,b):
+        return a - b
+print(minus(8,2))
+
+def product(a,b):
+        return a * b
+print(product(2,2))
+
+def divison(a,b):
+        return(a / b)
+print(divison(8,2))
